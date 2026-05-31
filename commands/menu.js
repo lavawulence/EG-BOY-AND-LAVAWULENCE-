5dfc9d4034eef118d2141623360450d7eb1dca28
lavawulence <debouangabriceamour@gmail.com>
2026-05-31 22:29:21 +0100

    Create commands/menu.js

---
+++ commands/menu.js
@@ -0,0 +1,18 @@
+export const MENU = `
+╔════════════════════╗
+   🤖 EG BOY MAKE ALL
+╚════════════════════╝
+
+👥 Groupes
+🎵 Média
+🎨 Création
+🤖 IA
+🎮 Jeux
+🛠️ Outils
+📢 Réseaux
+
+.help
+.menu
+.owner
+.ping
+`;
